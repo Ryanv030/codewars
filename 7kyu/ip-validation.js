8981424c90f30ed7285d43678bc8a82b5256fc93
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-10-15 22:42:16 -0700

    another solution

--- 7kyu/ip-validation.js
+++ 7kyu/ip-validation.js
@@ -3,23 +3,14 @@
 // Input to the function is guaranteed to be a single string.
 
 function isValidIP(str) {
-  console.log(str);
-  const newStr = str.split(".");
-  let valid = true;
-  const validate = newStr.forEach(str => {
-    let numStr = Number(str);
-    if (numStr < 256 && numStr > 0 && str[0] !== 0 && str[0] !== " ") {
-      return true;
-    } else {
-      valid = false;
-    }
-  });
-  if (newStr.length === 4 && valid === true) {
-    return true;
-  } else return false;
+  return (
+    str.split(".").filter(function(v) {
+      return v == Number(v).toString() && Number(v) < 256;
+    }).length == 4
+  );
 }
 
-console.log(isValidIP(" 1.2.3.4"));
+console.log(isValidIP("1e0.1e1.1e2.2e2"));
 // console.log(isValidIP("123.045.067.089"));
 // console.log(isValidIP("1.2.3.4"));
 // console.log(isValidIP("123.45.67.89"));
