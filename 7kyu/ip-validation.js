a2a6d6cf6a46fcd458a4ab456990968e630b165e
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-10-15 22:42:40 -0700

    another solution

--- 7kyu/ip-validation.js
+++ 7kyu/ip-validation.js
@@ -4,7 +4,7 @@
 
 function isValidIP(str) {
   return (
-    str.split(".").filter(function(v) {
+    str.split(".").filter(v => {
       return v == Number(v).toString() && Number(v) < 256;
     }).length == 4
   );
